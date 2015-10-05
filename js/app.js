08984da29408ed4da580633850722dc5de0d8964
byrenx <jofel.bayron@cloudsherpas.com>
2015-10-05 15:11:25 +0800

    modify center and add Polygon class

--- js/app.js
+++ js/app.js
@@ -2,109 +2,149 @@
     var Map = null;
 
     var Config = {
-	     user_name: 'byrenx',
+	    account: 'byrenx',
+        KEY: 'b5d8421cb6b5e43f2eedb00de552f7e639b2a5dd'
     }
 
 
     var defaultLayer = {
-	     user_name: Config.user_name,
-	     type: 'cartodb',
-	     sublayers: [{
-              		  sql: "SELECT * FROM stores",
-              		  cartocss: '#stores {marker-fill: #FF0000; marker-opacity: 0.5; marker-width: 50; marker-height: 50}'
-          	       },
-                   {
-            	    	sql: "SELECT * FROM new_orders",
-            	    	cartocss: Style.by_age_style
-  	               }
-  	              ]
+	    user_name: Config.account,
+	    type: 'cartodb',
+	    sublayers: [
+            {
+                sql: "SELECT * FROM stores",
+                cartocss: '#stores {marker-fill: #FF0000; marker-opacity: 0.5; marker-width: 50; marker-height: 50}'},
+		    {
+            	sql: "SELECT * FROM new_orders",
+            	cartocss: Style.by_age_style
+  		    }
+  	    ]
     };
 
+    // var Marker = {
+    //     markers: [],
+    //     init: function(){
+    //         this.markers = [];
+    //     },
+    //     add: function(feature){
+    //         var coord = feature.geometry.coordinates;
+    //         var marker = new google.maps.Marker({
+    //             map: map,
+    //             icon: feature.icon,
+    //         });
 
+    //         marker.setPosition({
+    //             lat: parseFloat(coord[1]),
+    //             lng: parseFloat(coord[0])
+    //         });
+    //         this.addInfoWindow(marker, feature.message);
+    //         this.markers.push(marker);
+    //     },
+    //     addInfoWindow: function(marker, message){
+    //         var infoWindow = new google.maps.InfoWindow({
+    //             content: message
+    //         });
 
-    var Marker = {
-        markers: [],
-        init: function(){
-            this.markers = [];
-        },
-        add: function(feature){
-            var coord = feature.geometry.coordinates;
-            var marker = new google.maps.Marker({
-                map: map,
-                icon: feature.icon,
-            });
+    //         google.maps.event.addListener(marker, 'click', function() {
+    //             infoWindow.open(map, marker);
+    //         });
+    //     },
+    //     list: function(){
+    //         return markers;
+    //     },
+    //     clear: function(){
+    //         for (var i=0; i<this.markers.length; i++){
+    //             this.markers[i].setMap(null);
+    //         }
+    //         this.markers = [];
+    //     }
+    // };
 
-            marker.setPosition({
-                lat: parseFloat(coord[1]),
-                lng: parseFloat(coord[0])
-            });
-            this.addInfoWindow(marker, feature.message);
-            this.markers.push(marker);
-        },
-        addInfoWindow: function(marker, message){
-            var infoWindow = new google.maps.InfoWindow({
-                content: message
-            });
-
-            google.maps.event.addListener(marker, 'click', function() {
-                infoWindow.open(map, marker);
-            });
-        },
-        list: function(){
-            return markers;
-        },
-        clear: function(){
-            for (var i=0; i<this.markers.length; i++){
-                this.markers[i].setMap(null);
+    var Polygon = {
+        polygons: {},
+        add: function(key, coordinates){
+            var points = [];
+            for (var i = 0; i < coordinates.length; i++) {
+                var coord = coordinates[i];
+                points.push({
+                    lat: coord[1],
+                    lng: coord[0]
+                });
             }
-            this.markers = [];
+
+            this.polygons[key] = new google.maps.Polygon({
+                paths: points,
+                strokeColor: '#FFFF00',
+                strokeOpacity: 0.8,
+                strokeWeight: 1
+            });
+            this.polygons[key].setMap(Map);
+        },
+        get: function(key){
+            return this.polygons[key];
+        },
+        all: function(){
+            return this.polygons;
         }
     };
 
     var POC = {
-       layers: [],
-       init : function(){
-        Map = new google.maps.Map(document.getElementById('map'),{
-                zoom: 8,
-        		    center: {lat:47.29413372501023,
-        			           lng:-112.32421875
-        			  }
-        });
-        this.initLayers();
-       },
-       initLayers: function(){
-         cartodb.createLayer(Map, defaultLayer)
-         .addTo(Map)
-         .on('done', function(layer) {
-           POC.createSelector(layer.getSubLayer(1));
-         })
-         .on('error', function(err) {
-          alert("some error occurred: " + err);
-         });
-       },
-       createSelector: function(layer) {
-        var cartocss = "";
-        var $options = $(".layer_selector").find("li");
+	    layers: [],
+	    init : function(){
+            Map = new google.maps.Map(document.getElementById('map'),{
+                zoom: 13,
+        	    center: {lat:39.738841359645896,
+        		         lng:-104.99093055725098
+        		        }
+            });
+            //this.initializePolygons();
+            this.initLayers();
+	    },
+	    initializePolygons: function() {
+	        var query = "SELECT * FROM colorado_1";
+            var url = 'https://'+Config.account+'.cartodb.com/api/v2/sql?format=GeoJSON&q='+query+'&api_key='+Config.KEY;
+	        
+            $.getJSON(url)
+                .done(function(data) {
+                    console.log(data);
+                    $.each(data.features, function(key, feature) {
+                        Polygon.add(feature.properties.gridcode, feature.geometry.coordinates[0][0]);
+                    });
+                });
+        },
+	    initLayers: function(){
+            cartodb.createLayer(Map, defaultLayer)
+		        .addTo(Map)
+		        .on('done', function(layer) {
+		            POC.createSelector(layer.getSubLayer(1));
+		        })
+		        .on('error', function(err) {
+		            alert("some error occurred: " + err);
+		        });
+	    },
+	    createSelector: function(layer) {
+            var cartocss = "";
+            var $options = $(".layer_selector").find("li");
 
-        $options.click(function(e) {
-            var $li = $(e.target);
-            var type = $li.data('type');
-            var selected = $li.attr('data');
+            $options.click(function(e) {
+		        var $li = $(e.target);
+		        var type = $li.data('type');
+		        var selected = $li.attr('data');
 
-            console.log(type);
+		        console.log(type);
 
-            $options.removeClass('cartocss_selected');
-            $li.addClass('cartocss_selected');
+		        $options.removeClass('cartocss_selected');
+		        $li.addClass('cartocss_selected');
 
-            if (type=="cartocss"){
-              cartocss = Style[selected];
-              layer.setCartoCSS(cartocss);
-            }else{
-              layer.setSQL("SELECT * FROM new_orders WHERE " + selected);
-            }
-        });
-       }
-   }//end of POC
+		        if (type=="cartocss"){
+		            cartocss = Style[selected];
+		            layer.setCartoCSS(cartocss);
+		        }else{
+		            layer.setSQL("SELECT * FROM new_orders WHERE " + selected);
+		        }
+            });
+	    }
+    }//end of POC
 
     POC.init();
 })(Style);
--- js/style.js
+++ js/style.js
@@ -1,39 +1,40 @@
 var Style = {by_gender_style: "#new_orders[gender='F'] {  \
-															marker-fill: #A6CEE3; \
-															} \
-															#new_orders[gender='M'] { \
-															marker-fill: #1F78B4; \
-															}",
-	     			by_age_style: "#new_orders[age=34] {marker-fill: #A6CEE3; \
-														} \
-														#new_orders[age=35] { \
-														marker-fill: #1F78B4; \
-														} \
-														#new_orders[age=40] { \
-														marker-fill: #B2DF8A; \
-														} \
-														#new_orders[age=48] { \
-														marker-fill: #33A02C; \
-														} \
-														#new_orders[age=50] { \
-														marker-fill: #FB9A99; \
-														} \
-														#new_orders[age=52] { \
-														marker-fill: #E31A1C; \
-														} \
-														#new_orders[age=53] { \
-														marker-fill: #FDBF6F; \
-														} \
-														#new_orders[age=56] { \
-														marker-fill: #FF7F00; \
-														} \
-														#new_orders[age=58] { \
-														marker-fill: #CAB2D6; \
-														} \
-														#new_orders[age=66] { \
-														marker-fill: #6A3D9A; \
-														} \
-														#new_orders { \
-														marker-fill: #DDDDDD; \
-														}"
-	    			};
+marker-fill: #A6CEE3; \
+} \
+#new_orders[gender='M'] { \
+marker-fill: #1F78B4; \
+}",
+	     	 by_age_style: "#new_orders[age=34] {marker-fill: #A6CEE3; \
+} \
+#new_orders[age=35] { \
+marker-fill: #1F78B4; \
+} \
+#new_orders[age=40] { \
+marker-fill: #B2DF8A; \
+} \
+#new_orders[age=48] { \
+marker-fill: #33A02C; \
+} \
+#new_orders[age=50] { \
+marker-fill: #FB9A99; \
+} \
+#new_orders[age=52] { \
+marker-fill: #E31A1C; \
+} \
+#new_orders[age=53] { \
+marker-fill: #FDBF6F; \
+} \
+#new_orders[age=56] { \
+marker-fill: #FF7F00; \
+} \
+#new_orders[age=58] { \
+marker-fill: #CAB2D6; \
+} \
+#new_orders[age=66] { \
+marker-fill: #6A3D9A; \
+} \
+#new_orders { \
+marker-fill: #DDDDDD; \
+}",
+             heatmap: ''
+	    	};
